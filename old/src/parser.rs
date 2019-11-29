69d7d6180a7ac98d9b6b821676c813f06c4ed655
Harrison Totty <harrisont@wolfram.com>
2019-11-29 14:47:50 -0600

    Clean slate

--- old/src/parser.rs
+++
@@ -1,2 +0,0 @@
-//! Contains functions concerning parsing various file formats.
-
--- old/src/simulation.rs
+++
@@ -1,9 +0,0 @@
-//! Contains the logic concerning simulation objects.
-
-use specs::prelude::*;
-
-/// Represents a simulation.
-pub struct Simulation {
-    world: World
-}
-
